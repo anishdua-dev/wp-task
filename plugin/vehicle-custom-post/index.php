ea01aaac1a65e2d91c41f41d54464203eba3e7d3
anishdua-dev <81799922+anishdua-dev@users.noreply.github.com>
2021-04-05 14:48:23 +0530

    Fixture to ajax hook

--- plugin/vehicle-custom-post/index.php
+++ plugin/vehicle-custom-post/index.php
@@ -24,7 +24,6 @@ class WPVehiclePostType {
 
     public function __construct() {
         register_activation_hook( __FILE__, array($this, 'booking_table_on_activation') );
-        // add_action( 'init', array($this, 'booking_table_on_activation') );
         add_action( 'init', array($this, 'vehicle_custom_post_type') );
         add_action( 'add_meta_boxes', array($this, 'wpt_add_vehicle_metaboxes') );
         add_action( 'save_post', array($this,'wpt_save_vehicle_meta'), 1, 2 );
@@ -33,6 +32,22 @@ class WPVehiclePostType {
         add_action( 'admin_menu', array($this, 'register_booking_menu_page') );
         add_action('admin_enqueue_scripts', array($this,'my_enqueue'));
 
+        add_action("wp_ajax_get_vechile_by_cat", array($this, "get_vechile_by_cat"));
+        add_action("wp_ajax_nopriv_get_vechile_by_cat", array($this, "get_vechile_by_cat"));
+
+        add_action("wp_ajax_get_vechile_price", array($this, "get_vechile_price"));
+        add_action("wp_ajax_nopriv_get_vechile_price", array($this, "get_vechile_price"));
+
+        add_action("wp_ajax_send_booking_form_action", array($this, "send_booking_form_action"));
+        add_action("wp_ajax_nopriv_send_booking_form_action", array($this, "send_booking_form_action"));
+
+        add_action("wp_ajax_change_booking_status", array($this, "change_booking_status"));
+        add_action("wp_ajax_nopriv_change_booking_status", array($this, "change_booking_status"));
+
+        add_action( 'phpmailer_init', array($this, 'send_smtp_email' ));
+        
+        add_action( 'wp_mail_failed', array($this, 'onMailError'), 10, 1 );
+        
     }
 
     public function booking_table_on_activation(){
@@ -223,6 +238,197 @@ class WPVehiclePostType {
             $tablename1 = $wpdb->prefix.'posts';
             $resData = $wpdb->get_results("SELECT * FROM $tablename sb LEFT JOIN $tablename1 p ON sb.vehicle_id = p.ID", ARRAY_A);
            include_once('admin/booking.php');
+        }
+
+        public function get_vechile_by_cat(){
+            global $post;
+            $json = array();
+            if(!empty($_POST['cat_id'])){
+                $cat_id = $_POST['cat_id'];
+                $args = array(
+                    'numberposts' => -1,
+                    'tax_query' => array(
+                        array(
+                        'taxonomy' => 'vehicle_type',
+                        'field' => 'term_id',
+                        'terms' => $cat_id
+                         )
+                        ),
+                    'post_type'   => 'vehicles'
+                  );
+                   
+                  $vehicles_by_cat = get_posts( $args );
+                  if(!empty($vehicles_by_cat)){
+                    $json['success'] = true;
+                    $json['data'] = $vehicles_by_cat;
+                  }else{
+                    $json['success'] = false;
+                    $json['data'] = array();
+                  }
+                 
+            }else{
+                $json['success'] = false;
+                $json['data'] = array();
+            }
+            echo json_encode($json);
+            die();
+        }
+
+        public function get_vechile_price(){
+            $json = array();
+            if(!empty($_POST['vehicle_id'])){
+                $vehicle_price = get_post_meta( $_POST['vehicle_id'], 'start_price', true ); 
+                if(!empty($vehicle_price)){
+                    $json['success'] = true;
+                    $json['data'] = $vehicle_price;
+                }else{
+                    $json['success'] = false;
+                    $json['data'] = array();
+                }
+        
+            }else{
+                $json['success'] = false;
+                $json['data'] = array();
+            }
+            echo json_encode($json);
+            die();
+        }
+
+        public function send_booking_form_action(){
+            // echo '<pre>'; print_r($_POST); die();
+            global $wpdb;
+            $json = array();
+            $tablename = $wpdb->prefix.'save_bookings';
+            if(isset($_POST['action']) && $_POST['action'] == 'send_booking_form_action'){
+                $save_booking = $wpdb->insert( $tablename, array(
+                    'first_name' => $_POST['first_name'], 
+                    'last_name' => $_POST['last_name'],
+                    'email' => $_POST['email'], 
+                    'phone' => $_POST['phone'],
+                    'vehicle_type' => $_POST['vehicle_type'], 
+                    'vehicle_id' => $_POST['vehicle_name'], 
+                    'price' => $_POST['vehicle_price'],
+                    'message' => $_POST['message']
+                ),
+                    array( '%s', '%s', '%s', '%s', '%d', '%d', '%s', '%s')
+                );
+        
+                if($save_booking){
+                    $message = '';
+                    $message .= '<html>';
+                    $message .= '<head>';   
+                    $message .= '<title>New Booking</title>';
+                    $message .= '</head>';
+                    $message .= '<body>';
+                    $message .= '<p>New Booking is made</p>';
+                    $message .= '<table>';
+                        $message .= '<tr>';
+                            $message .= '<th>Booking ID</th>';
+                            $message .= '<th>Booking Status</th>';
+                        $message .= '</tr>';
+                        $message .= '<tr>';
+                            $message .= '<td>'.$save_booking.'</td>';
+                            $message .= '<td>Pending</td>';
+        
+                        $message .= '</tr>';
+                    $message .= '</table>';
+                    $message .= '</body>';
+                    $message .= '<html>';
+                    $headers = array('Content-Type: text/html; charset=UTF-8');
+        
+                    // Admin Email
+                    $to = get_option( 'admin_email' );
+                    $subject = 'New Booking';           
+                
+                    wp_mail( $to, $subject, $message, $headers );
+        
+                    // Customer Email
+                    $to = $_POST['email'];
+                    wp_mail( $to, $subject, $message, $headers );
+        
+                    $json['success'] = true;
+                    $json['data'] = $save_booking;
+                }else{
+                    $json['success'] = false;
+                    $json['data'] = array();
+                }
+            }
+            echo json_encode($json);
+            die();
+        }
+
+        public function change_booking_status(){
+            global $wpdb;
+            $json = array();
+            $table_name = $wpdb->prefix.'save_bookings';
+        
+            $update_req = $wpdb->update( 
+                $table_name,
+                array( 'status' =>$_POST['status']),
+                array( 'id' => $_POST['booking_id'] ) 
+            );
+            if($update_req){
+                if($_POST['status'] == 0){
+                    $status = 'Pending';
+                }elseif($_POST['status'] == 1){
+                    $status = 'Approved';
+                }elseif($_POST['status'] == 2){
+                    $status = 'Reject';
+                }else{
+                    $status = 'Completed';
+                }
+                $message = '';
+                    $message .= '<html>';
+                    $message .= '<head>';   
+                    $message .= '<title>Booking Status update</title>';
+                    $message .= '</head>';
+                    $message .= '<body>';
+                    $message .= '<p>Your booking status is updated.</p>';
+                    $message .= '<table>';
+                        $message .= '<tr>';
+                            $message .= '<th>Booking ID</th>';
+                            $message .= '<th>Booking Status</th>';
+                        $message .= '</tr>';
+                        $message .= '<tr>';
+                            $message .= '<td>'.$_POST['booking_id'].'</td>';
+                            $message .= '<td>'. $status .'</td>';
+        
+                        $message .= '</tr>';
+                    $message .= '</table>';
+                    $message .= '</body>';
+                    $message .= '<html>';
+                    $headers = array('Content-Type: text/html; charset=UTF-8');
+        
+                    // Admin Email
+                    $to = get_option( 'admin_email' );
+                    $subject = 'Booking status updated';           
+                
+                    wp_mail( $to, $subject, $message, $headers );
+                $json['success'] = true;
+                $json['data'] = $update_req;
+            }else{
+                $json['success'] = false;
+                $json['data'] = array();
+            }
+            echo json_encode($json);
+            die();  
+        }
+
+        public function send_smtp_email( $phpmailer ) {
+            $phpmailer->isSMTP();
+            $phpmailer->Host       = 'smtp.gmail.com';
+            $phpmailer->Port       = '587';
+            $phpmailer->SMTPSecure = 'tls';
+            $phpmailer->SMTPAuth   = true;
+            $phpmailer->Username   = 'moudgil.developer@gmail.com';
+            $phpmailer->Password   = 'radhey@92';
+            $phpmailer->From       = 'flyhighdua@gmail.com';
+            $phpmailer->FromName   = 'Anish';
+        }
+        public function onMailError( $wp_error ) {
+            echo "<pre>";
+            print_r($wp_error);
+            echo "</pre>";
         }
 }
 
